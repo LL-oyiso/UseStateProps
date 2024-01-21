c4e349319b0759df2ec70b7cf0ed70e539fafb4f
LL-oyiso <theo.tose.tt@gmail.com>
2024-01-21 21:05:41 +0200

    Functional variable added

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,25 +1,28 @@
 import React from 'react'
 
-function Card(){
-<figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
-  <img className="w-24 h-24 md:w-48 md:rounded-none rounded-full mx-auto" src="/sarah-dayan.jpg" alt="" width="384" height="512" />
-  <div className="pt-6 md:p-8 text-center space-y-4">
-    <blockquote>
-      <p className="text-lg font-medium">
-        “Tailwind CSS is the only framework that I have seen scale
-        on large teams. It is easy to customize, adapts to any design,
-        and the build size is tiny.”
-      </p>
-    </blockquote>
-    <figcaption className="font-medium">
-      <div className="text-sky-500 dark:text-sky-400">
-        Sarah Dayan
-      </div>
-      <div className="text-slate-700 dark:text-slate-500">
-        Staff Engineer, Algolia
-      </div>
-    </figcaption>
-  </div>
-</figure>
+function Card({username = " Loyiso " , post = " Front end dev "}){
+   
+    return(<figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
+    <img className="w-24 h-24 md:w-48 md:rounded-none rounded-full mx-auto" src="https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" width="384" height="512" />
+    <div className="pt-6 md:p-8 text-center space-y-4">
+      <blockquote>
+        <p className="text-lg font-medium">
+          “Tailwind CSS is the only framework that I have seen scale
+          on large teams. It is easy to customize, adapts to any design,
+          and the build size is tiny.”
+        </p>
+      </blockquote>
+      <figcaption className="font-medium">
+        <div className="text-sky-500 dark:text-sky-400">
+         {username}
+        </div>
+        <div className="text-slate-700 dark:text-slate-500">
+       {post}
+        </div>
+      </figcaption>
+    </div>
+  </figure>)
+
 }
+
 export default Card
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-
+import './components/Card.jsx'
+import Card from './components/Card.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <Card username = "Loyd"/>
+    <Card  post ="Staff Engg."/>
+    <Card username ="Boss" post ="Zonke" />
   </React.StrictMode>,
 )
